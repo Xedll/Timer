256d6ada0f8e02629ef517e416fb8a6483148446
Xedll <97981835+Xedll@users.noreply.github.com>
2022-12-07 21:27:54 +0300

    Fixes
    Fixed time display and signal work

--- Project/js/main.js
+++ Project/js/main.js
@@ -6,14 +6,13 @@ let time = 0;
 let count = 0;
 
 setTimeout(function run() {
-   console.log(time)
-   console.log(count)
    showTime(time)
    time -= 1;
    if (time < 0) {
-      console.log(12)
       time = 0;
-      if (count) {
+      if (count > 0) {
+         console.log(count)
+
          signal.play()
       }
       setTimeout(run, 1000);
@@ -24,9 +23,9 @@ setTimeout(function run() {
 
 
 const showTime = (time) => {
-   const hours = Math.floor(time / 3600);
-   const minutes = Math.floor((time - hours * 3600) / 60);
-   const seconds = time - hours * 3600 - minutes * 60;
+   const hours = Math.floor(time / 3600) < 9 ? '0' + Math.floor(time / 3600) : Math.floor(time / 3600);
+   const minutes = Math.floor((time - hours * 3600) / 60) < 9 ? '0' + Math.floor((time - hours * 3600) / 60) : Math.floor((time - hours * 3600) / 60);
+   const seconds = (time - hours * 3600 - minutes * 60) < 9 ? '0' + (time - hours * 3600 - minutes * 60) : (time - hours * 3600 - minutes * 60);
    document.querySelector('.main__time').textContent = hours + ':' + minutes + ':' + seconds;
    document.title = hours + ':' + minutes + ':' + seconds;
 }
@@ -37,10 +36,8 @@ document.querySelector('.main__buttonSet').onclick = () => {
          i.value *= -1;
       }
    }
-   console.log(time)
    count++
    time = Math.ceil(+inputs[0].value) * 3600 + Math.ceil(+inputs[1].value) * 60 + Math.ceil(+inputs[2].value);
-   console.log(time)
 }
 
 document.querySelector('.main__buttonStop').onclick = () => {
@@ -48,7 +45,7 @@ document.querySelector('.main__buttonStop').onclick = () => {
       i.value = '';
    }
    document.title = "Timer";
-   --count;
+   count--;
    time = 0;
    signal.pause()
 }
